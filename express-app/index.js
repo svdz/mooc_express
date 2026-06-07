ddbdaf166a807ec28c8a46979c8087315c87ad70
SergDer <svdz@test.ip>
2026-06-07 20:20:47 +0200

    Fixed composer to use image from Hub, multi-platform builds

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!  This is changed version</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 const PORT = 8080
